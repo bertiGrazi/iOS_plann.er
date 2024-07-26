54492dc2e34b4f60fcfa96206e8261b258529525
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-07-26 12:04:20 -0300

    create project plann.er

---
+++ plann.er/plann.er/Custom/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  plann.er
+//
+//  Created by Grazi  Berti on 26/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
